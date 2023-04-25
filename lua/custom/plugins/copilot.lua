0ccaaf5b1e5c3e9d1052b653f989a12398fabf84
Roger Kind Kristiansen <roger.kristiansen@gmail.com>
2023-04-25 20:29:37 +0200

    Remap Copilot accept key

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,7 +1,14 @@
 -- [[ Configure Copilot ]]
+--
 vim.keymap.set('n', '<leader>c', ':Copilot<CR>', { desc= '[C]opilot' })
 vim.keymap.set('i', '<M-.>',     '<Plug>(copilot-next)', { desc = 'Copilot next suggestion'})
 vim.keymap.set('i', '<M-,>',     '<Plug>(copilot-previous)', { desc = 'Copilot previous suggestion'})
+
+-- Remap tab key to CTRL+J
+vim.g.copilot_no_tab_map = true
+vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+
+
 
 return {
     {
