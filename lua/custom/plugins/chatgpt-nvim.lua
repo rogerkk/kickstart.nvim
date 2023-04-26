1fb465498fbb6d9f67dc8e72491f763c7abedc6b
Roger Kind Kristiansen <roger.kristiansen@gmail.com>
2023-04-26 13:46:54 +0200

    Add chatgpt.nvim plugin

---
+++ lua/custom/plugins/chatgpt-nvim.lua
@@ -0,0 +1,16 @@
+return {
+  "jackMort/ChatGPT.nvim",
+    event = "VeryLazy",
+        config = function()
+            require("chatgpt").setup({
+                popup_input = {
+                    submit = "<C-s>",
+                },
+            })
+        end,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim"
+    }
+}
