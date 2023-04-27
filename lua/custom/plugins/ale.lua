b443a7265425cdbd0779c983cbc482e02aee00dc
Roger Kind Kristiansen <roger.kristiansen@gmail.com>
2023-04-27 09:18:25 +0200

    Add ALE for linting

---
+++ lua/custom/plugins/ale.lua
@@ -0,0 +1,25 @@
+--vim.cmd([[ let b:ale_fixers = { '*': ['remove_trailing_lines', 'trim_whitespace',\
+--'ruby': ['rubocop', 'reek'],\
+--'erb': ['erblint'],\
+--'javascript': ['eslint'] } ]])
+
+vim.b.ale_fixers = {
+--  '*' = {'remove_trailing_lines', 'trim_whitespace'},
+  ruby = {'rubocop'},
+  erb =  {'erblint'},
+  javascript = {'eslint'},
+}
+
+vim.b.ale_linters = {
+  ruby = {'rubocop', 'reek'}
+}
+
+vim.g.ale_sign_error   = '❌'
+vim.g.ale_sign_warning = '⚠️ '
+vim.g.ale_fix_on_save  = 1
+
+return {
+    {
+        "dense-analysis/ale",
+    },
+}
