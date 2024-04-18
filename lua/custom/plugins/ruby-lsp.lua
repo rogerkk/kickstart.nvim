4810548c8360f7e032f4273c607a05371f14caf4
Roger Kind Kristiansen <roger.kristiansen@gmail.com>
2024-04-18 14:34:18 +0200

    Add Ruby-LSP setup for nvim

---
+++ lua/custom/plugins/ruby-lsp.lua
@@ -0,0 +1,13 @@
+return {
+  -- Utilize nvim-lspconfig for simple basic setup
+  require'lspconfig'.ruby_lsp.setup{
+    settings = {
+      
+    },
+    on_attach = function()
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=0})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer=0})
+      vim.keymap.set("n", "<leader>c", vim.lsp.buf.code_action, {buffer=0})
+    end,
+  }
+}
